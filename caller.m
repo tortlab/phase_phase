2abdf12b06d460d5638da0328be12949ef50d81b
Computational Neurophysiology Laboratory <robsonscheffer@neuro.ufrn.br>
2016-08-08 13:39:33 -0300

    Update caller.m

--- caller.m
+++ caller.m
@@ -1,8 +1,6 @@
-%% Routine caller
+%% Caller routine
 
-% This routine calls the main methods used in the paper of
-% Scheffer-Teixeira & Tort (2016) published in eLife.
-
+% This routine calls the main methods used in Scheffer-Teixeira & Tort, eLife 2016.
 
 % Select folder where files are located and add path
 path = uigetdir;
