c4c35e900dab62a143adb2450540891e46851bff
Computational Neurophysiology Laboratory <tortlab@users.noreply.github.com>
2016-11-15 13:56:45 +0100

    Update phase_phase_core.m

--- phase_phase_core.m
+++ phase_phase_core.m
@@ -1,4 +1,4 @@
 function [output] = phase_phase_core(phase1,phase2,m)
 
-        output = mean(exp(i*(m*(phase1) - (phase2))));
+        output = mean(exp(i*(m*(unwrap(phase1)) - unwrap(phase2))));
 end
